e1813b82fb4d4ff8350de0e2a8bf3432a02ea865
Pablo Hernández <ppblo.hernandez@gmail.com>
2024-04-17 13:05:40 -0400

    dia 8 uml

---
+++ Dia8/Model/Pelota.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Pelota():
+    def __init__(self):
+        self.color = None
+        self.material = None
+
+    def rebotar(self, altura):
+        pass
+
+    def imprimit(self, ):
+        pass
+
