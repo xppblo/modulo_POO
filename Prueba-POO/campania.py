05a6954c324b5a4c1d87975633603a516ba0fb57
Pablo Hernández <ppblo.hernandez@gmail.com>
2024-04-25 20:24:30 -0400

    Avance prueba poo

--- Prueba-POO/campania.py
+++ Prueba-POO/campania.py
@@ -7,10 +7,43 @@ class Campania():
         self.__fecha_termino = fecha_termino
         self.__anuncios = [self.instancias_de_anuncios() for dicc in anuncios]
         
+    @property
+    def nombre(self):
+        return self.__nombre
+    
+    @nombre.setter
+    def ancho(self, nombre : str):
+        self.__nombre = nombre
+        
+    @property
+    def fecha_inicio(self):
+        return self.__fecha_inicio
+    
+    @fecha_inicio.setter
+    def fecha_inicio(self, fecha_inicio : str):
+        self.__fecha_inicio = fecha_inicio
+    
+    @property
+    def fecha_termino(self):
+        return self.__fecha_termino
+    
+    @fecha_termino.setter
+    def ancho(self, fecha_termino : str):
+        self.__fecha_termino = fecha_termino
+        
+    @property
+    def anuncios(self):
+        return self.__anuncios
+        
+    
+        
     def instancias_de_anuncios(self, anuncio : dict):
         return Video()
         return Display()
         return Social()
     
     def __str__(self) -> str:
-        pass
+        return """
+        Nombre de la campaña:
+        Anuncios:
+        """
